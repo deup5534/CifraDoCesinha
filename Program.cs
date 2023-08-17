676b1eb21f610b5f389906b8982c3508cb2e1c35
Thiago Bezerra de Carvalho <112219943+deup5534@users.noreply.github.com>
2023-08-17 09:53:24 -0300

    Finalizado

--- Program.cs
+++ Program.cs
@@ -1,29 +1,30 @@
-Console.WriteLine("---------CIFRA DE CESAR---------\n");
+Console.WriteLine("---------CIFRA DE CESAR---------\n"); //Exibindo nome do programa.
 
 
 Console.Write("Escreva aqui sua senha, vou te mostrar como ela ficaria criptografada.\n");
-string segredoASerEscrito = Console.ReadLine();
+string segredoASerEscrito = Console.ReadLine(); //capturando a palavra que vai ser criptografada
 
 Console.Write("Agora, a chave que você deseja. (Por favor, digite de 1 até 27).\n");
-int chave = Convert.ToInt32(Console.ReadLine());
+int chave = Convert.ToInt32(Console.ReadLine()); //denominando qual chave da criptografia será
 
 
-if (chave >= 1 && chave <= 26)
+if (chave >= 1 && chave <= 26) //Colocando condições para que haja limite na chave.
 
 {
-    Console.WriteLine("**CRIPTOGRAFANDO**");
+    Console.WriteLine("**CRIPTOGRAFANDO**"); //"Tela de loading".
 
-    Thread.Sleep(2000);
+    Thread.Sleep(2000); //Tempo de 2 segundos.
 
-    string cifraDeCesar = Criptografar(segredoASerEscrito, chave);
+    string cifraDeCesar = Criptografar(segredoASerEscrito, chave); //Combinando os resultados e com o chave que o usuário decidiu.  
 
-    Console.Write($"Sua mensagem criptografada: {cifraDeCesar}\n");
+    Console.Write($"Sua mensagem criptografada: {cifraDeCesar}\n"); //Exibindo o resultado da combinação anterior.
 }
 
-else{
+else
+{
 
-        Console.WriteLine("\n'-'\nCheio das graças, né?");
-
+        Console.WriteLine("\n'-'\nCheio das graças, né?"); 
+                                                            //Exibindo tela de erro.
         Console.WriteLine("\nEscreve certo isso...");
 
 }
